84cc567d33e22d7ab5bd2097b93a86a6d4454790
DongvinPark <dongvin99@naver.com>
2025-11-13 22:12:11 +0900

    implemented json data fetching function

--- src/components/downloader/JsonFileDownloader.tsx
+++ src/components/downloader/JsonFileDownloader.tsx
@@ -7,22 +7,64 @@ type JsonFileDownloaderProps = {
     sortedIndicators: Record<string, string[]>;
 }
 
+type GraphData = Map<
+    string, {time: string, value: number}[]
+>;
+
 async function downloadJsonFilesForGraph(
     {appMeta, currentLang, duration, sortedIndicators}: JsonFileDownloaderProps
 ){
     // 실제 api 호출 진행
-    console.log("!!! 앱메타 !!!");
-    console.log(appMeta);
-    console.log("!!! 현재 언어 !!!");
-    console.log(currentLang);
-    console.log("!!! 기간 !!!");
-    console.log(duration);
-    console.log("!!! 인덱스 맵 !!!");
-    console.log(sortedIndicators);
+    // event fetch는 나중에 BE 구축되면 한다.
+    // TODO : 404 not found, 각종 예외, 없는 파일들에 대한 처리, 지나치게 오래 걸릴 경우의 처리등
+    // TODO : 각종 엣지 케이스들에 대응하면서 coucurrency 를 도입해야 한다.
+    const currentYear: number = new Date().getFullYear();
+    const cdnRoot: string = appMeta['cdn-root-url'];
 
-    // 1.5초(1500ms) 강제 지연 : API 호출로 인한 네트워크 지연 
-    await new Promise(resolve => setTimeout(resolve, 1500));
-    console.log("!!! 네트워킹 끝 !!!");
+    var resultMap: GraphData = new Map();
+    const len: number = Object.entries(sortedIndicators).length;
+    for(var i = 0; i<len; i++){
+        var jsonArr: {time: string, value: number}[] = [];
+        const indexName: string = Object.entries(sortedIndicators)[i][0];
+        const categoryName: string = sortedIndicators[indexName][0];
+        for(
+            var startYear = currentYear - duration;
+            startYear<currentYear + 1;
+            startYear++
+        ){
+            var reqUrl = cdnRoot;
+            if(startYear < currentYear){
+                reqUrl += ( 
+                    "past-year/" + categoryName + "/" + indexName + "/"
+                    + String(startYear) + "-" + indexName + ".json"
+                );
+            } else {
+                reqUrl += ( 
+                    "this-year/" + categoryName + "/" + indexName + "/"
+                    + String(startYear) + "-" + indexName + ".json"
+                );
+            }
+            const response = await fetch(reqUrl);
+            if (!response.ok){
+                throw new Error(`HTTP error! status : ${response.status}`);
+            }
+            const rawData = await response.json();
+            var converted;
+            if(rawData.length > 0 && "value" in rawData[0]){
+                converted = rawData;
+            } else {
+                converted = rawData.map(({ time, close }) => ({ time, value: close }));
+            }
+            jsonArr.push(...converted);
+        }//inner for
+
+        // put to Map
+        resultMap.set(indexName, jsonArr);
+    }//outer for
+
+    console.log("!!! api fetch 결과 테스트용 !!!");
+    console.log(resultMap);
+    return resultMap;
 }
 
 export default downloadJsonFilesForGraph;
